b4e019dab34e106ee64952edb72795cf6ca5c975
a-wiles <98373402+a-wiles@users.noreply.github.com>
2022-05-01 13:26:17 -0400

    Added tables for department, employee and roll as well as parameters for each one

--- db/schema.sql
+++ db/schema.sql
@@ -2,4 +2,22 @@ DROP TABLE IF EXISTS employee;
 DROP TABLE IF EXISTS department;
 DROP TABLE IF EXISTS role;
 
-USE 
+CREATE TABLE employee (
+    id PRIMARY KEY INTEGER,
+    first_name VARCHAR (40),
+    last_name VARCHAR (40),
+    role_id INTEGER,
+    manager_id INTEGER NULL
+);
+
+CREATE TABLE department (
+    id PRIMARY KEY INTEGER,
+    name VARCHAR(40)
+);
+
+CREATE TABLE role (
+    id PRIMARY KEY INTEGER,
+    title VARCHAR(40),
+    salary DECIMAL,
+    department_id INTEGER
+)
