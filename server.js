07235981e7bd848aced1a321006dd5e33a789e15
a-wiles <98373402+a-wiles@users.noreply.github.com>
2022-05-01 14:59:14 -0400

    Added .then answers prompts to allow individual functions to be called

--- server.js
+++ server.js
@@ -20,29 +20,56 @@ const startMenu = function () {
                 'Update an employees role',
                 'Exit']
         }
-    ])
-}
+    ]).then((answers) => {
+        var { choices } = answers;
+        if (choices === "View all departments") {
+            viewAll();
+        } else if (choices === "View All Employees") {     
+            viewAllEmployees();
+        }else if (choices === "View all roles") {
+            viewAllRoles();
+        } else if (choices === "Add a department") {
+            addDepartment();
+        } else if (choices === "Add a role") {
+            addRole();
+        } else if (choices === "Add an employee") {
+            addEmployee();
+        } else if (choices === "Update an employee role") {
+            updateEmployee();
+        } else if (choices === "Exit") {
+            connection.end()
+        };
+        });
 
-function viewAll
+function viewAll () {};
 //Department Names and Department IDs
 
-function viewAllRoles
+function viewAllRoles () {};
 // fiew all Job titles, role id, department id and salary
 
-function viewAllEmployees
+function viewAllEmployees (){};
 // employee data, employee id, first name, last name, job titles, departments, salaries and managers
 
-function addDepartment
+function addDepartment () {
+console.log("Add Department")
+    Inquirer.prompt([
+        {
+            type:"input",
+            message:"Enter Department: ",
+            name:"department"
+        }
+    ]).then(userresponse => {
+        const newDepartment = new Department(userresponse.name);
+        startMenu()
+    })
+};
 //add department name
 
-function addRole
+function addRole () {};
 // name of role, salary & department
 
-function addEmployee
+function addEmployee () {};
 //add employee first, last, role, manager
 
-function updateEmployee
+function updateEmployee () {};
 // prompted to select employee to update + new role
-
-function exit
-//exit menu
