ab3f69d88d814acb0ba16a99d673271e196dd2c7
a-wiles <98373402+a-wiles@users.noreply.github.com>
2022-05-01 13:42:04 -0400

    Added start menu function which asks the multiple choice questions for adding, viewing or updating

--- server.js
+++ server.js
@@ -5,3 +5,20 @@ const app = express();
 const sequelize = require('../config/connection');
 require('console.table')
 
+const startMenu = function () {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'questions',
+            message: 'Please choose a following option',
+            choices: ['View all departments',
+                'View all employees',
+                'View all roles',
+                'Add a department',
+                'Add an employee',
+                'Add a role',
+                'Update an employees role',
+                'Exit']
+        }
+    ])
+}
